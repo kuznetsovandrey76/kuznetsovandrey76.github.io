67235f79c7cb2734d234903ca27b0702786caf84
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-06-14 16:45:11 +0300

    fix jquery DONE

--- js/main.js
+++
@@ -1,7 +0,0 @@
-alert(1);
-$(document).ready(function(){
-	$('.skills-git').hide();
-	$('.skills-open-git').click(function() {
-		$('.skills-git').toggle();
-	});    
-});
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-alert(1);
 $(document).ready(function(){
 	$('.skills-git').hide();
 	$('.skills-open-git').click(function() {
