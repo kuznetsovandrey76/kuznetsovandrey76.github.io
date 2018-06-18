a105e4869346406cd838739bdbc3443d07473e2c
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-06-18 23:52:22 +0300

    add dark mode

--- main.js
+++ main.js
@@ -2,5 +2,19 @@ $(document).ready(function(){
 	$('.skills-git').hide();
 	$('.skills-open-git').click(function() {
 		$('.skills-git').toggle();
-	});    
+	});  
+
+	$('.checkbox__dark-mode-outer').click(function() {
+		$('.flex').toggleClass('flex__dark-mode');
+		$('.profile-links__a').toggleClass('profile-links__a-dark');
+		$('.checkbox__dark-mode-inner').toggleClass('checkbox__dark-mode-inner-move');
+		if ($('.checkbox__dark-mode-text').html() === 'dark mode') {
+			$('.checkbox__dark-mode-text').html('light mode');
+		} else {
+			$('.checkbox__dark-mode-text').html('dark mode');
+		}
+	});  
+
+
+
 });
